bc99b51316e027b888d377930b8b62037f5efc6a
Paulo Gaspar <paulogaspar404@hotmail.com>
2015-07-08 04:50:00 -0300

    First commit

---
+++ Converter.java
@@ -0,0 +1,16 @@
+import java.io.BufferedWriter;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.IOException;
+import java.io.BufferedWriter;
+import java.io.OutputStreamWriter;
+import java.io.FileOutputStream;
+import java.util.ArrayList;
+
+public class Converter{
+	public static void main(String [] args) throws IOException{
+		ArrayList<ArrayList<String>> elements = new ArrayList<ArrayList<String>>();
+		BufferedWriter out = new BufferedWriter(new OutputStreamWriter(new FileOutputStream("texto.txt")));
+
+	}
+}
